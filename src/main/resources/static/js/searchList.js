360085cf73aa743f05acf21b4e168541748e0b4f
Dahee <dduosy9@gmail.com>
2023-03-14 17:02:43 +0900

    기능추가(클릭 시 isbn 전달), 리팩토링

--- src/main/resources/static/js/searchList.js
+++ src/main/resources/static/js/searchList.js
@@ -1,60 +1,66 @@
+/* 검색창에 입력한 값 받아오기 */
 const urlParams = new URL(location.href).searchParams;
-const bookSearch = urlParams.get('bookSearch');
+const bookSearch = urlParams.get("bookSearch");
 let ISBN;
 
 $.ajax({
-    method: "GET",
-    url: "https://dapi.kakao.com/v3/search/book?target=title",
-    data: { query: bookSearch, sort: "accuracy"},
-    headers: { Authorization: "KakaoAK a139cc1585a4fde2ef179e9e239f02a9" }
-})
-    .done(function( msg ) {
-        console.log(msg);
-        
-        let tagArea = document.getElementById('tagArea');
-        let total_result = document.getElementById('total_result');
-        let title = document.getElementsByTagName('title');
-        
-        $( total_result ).append(msg.meta.pageable_count);
-        $( title ).append(bookSearch + " | 검색 결과");
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: bookSearch, sort: "accuracy" },
+  headers: { Authorization: "KakaoAK a139cc1585a4fde2ef179e9e239f02a9" },
+}).done(function (msg) {
+  //console.log(msg);
 
-        for(let i=0; i<msg.documents.length; i++){
-            
-            let new_divTag = document.createElement('div');
-            new_divTag.setAttribute('class', 'book');
-            //new_divTag.setAttribute('title', msg.documents[i].title);
-            tagArea.appendChild(new_divTag);
+  let tagArea = document.getElementById("tagArea");
+  let total_result = document.getElementById("total_result");
+  let title = document.getElementsByTagName("title");
 
-            
-            //let new_divTag = document.getElementsByClassName('book');
-            $( new_divTag ).append("<div><img src='" + msg.documents[i].thumbnail + "'/></div>");
+  $(total_result).append(msg.meta.pageable_count); // 전체 건수
+  $(title).append(bookSearch + " | 검색 결과"); // html title
 
-            
-            let new_divTag2 = document.createElement('div');
-            new_divTag2.setAttribute('class', 'bookContents');
-            tagArea.appendChild(new_divTag2);
-            new_divTag.appendChild(new_divTag2);
-            
-            //let new_divTag2 = document.getElementsByClassName('bookContents');
+  // 검색된 문서 모두 출력
+  for (let i = 0; i < msg.documents.length; i++) {
+    // <div class="book"></div> 생성
+    let new_divTag = document.createElement("div");
+    new_divTag.setAttribute("class", "book");
+    tagArea.appendChild(new_divTag);
 
-            $( new_divTag2 ).append("<p><strong><a href='book_detail.html'>" + msg.documents[i].title + "</a></strong></p>");
-            $( new_divTag2 ).append("<p class='left'>" + msg.documents[i].authors + "</p>");
-            $( new_divTag2 ).append("<p class='left'>&middot</p>");
-            $( new_divTag2 ).append("<p class='left'>" + msg.documents[i].publisher + "</p>");
-            $( new_divTag2 ).append("<p>" + msg.documents[i].contents + "</p>");
+    // .book 태그에 이미지 넣기
+    $(new_divTag).append(
+      "<div><img src='" + msg.documents[i].thumbnail + "'/></div>"
+    );
 
-            console.log(msg.documents[i].title);
-            console.log(msg.documents[i].thumbnail);
-            ISBN = msg.documents[i].isbn;
-            console.log(ISBN);
+    //<div class="bookContents"></div> 생성
+    let new_divTag2 = document.createElement("div");
+    new_divTag2.setAttribute("class", "bookContents");
+    tagArea.appendChild(new_divTag2);
+    new_divTag.appendChild(new_divTag2);
 
-        }
+    // .bookContents 태그에 제목, 저자, 출판사, 책 소개 넣기
+    $(new_divTag2).append(
+      "<p><strong><a href='' class='book_link' data-isbn=" +
+        msg.documents[i].isbn +
+        ">" +
+        msg.documents[i].title +
+        "</a></strong></p>"
+    );
+    $(new_divTag2).append(
+      "<p class='left'>" + msg.documents[i].authors + "</p>"
+    );
+    $(new_divTag2).append("<p class='left'>&middot</p>");
+    $(new_divTag2).append(
+      "<p class='left'>" + msg.documents[i].publisher + "</p>"
+    );
+    $(new_divTag2).append("<p>" + msg.documents[i].contents + "</p>");
 
-        let a_tag ;
+    // 책 제목 클릭하면 해당 책의 isbn을 bookSearch에 넣어 URL에 넣기
+    let a_tag = document.getElementsByClassName("book_link");
 
-        
-        console.log(localStorage.getItem('isbn'));
-        
-    });
-
-    
+    for (let i = 0; i < a_tag.length; i++) {
+      a_tag[i].onclick = function (event) {
+        event.target.href =
+          "book_detail.html?bookSearch=" + event.target.dataset.isbn;
+      };
+    }
+  }
+});
