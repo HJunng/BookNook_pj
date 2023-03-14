04a94f950efc92eef09d0f091f916307de3979c6
hansumin <sumin_han@naver.com>
2023-03-14 13:14:44 +0900

    db연동만 완료

--- src/main/java/com/hll/booknook/controller/UserController.java
+++ src/main/java/com/hll/booknook/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.hll.booknook.controller;
 
+import com.hll.booknook.KakaoService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 @Controller
 @Slf4j
 public class UserController {
 
+    /*네이버*/
     @RequestMapping(value ="/login/naver", method = RequestMethod.GET)
     public String index(){
         log.info("home controller");
@@ -22,4 +28,26 @@ public class UserController {
         log.info("callback controller");
         return "NaverCallback";
     }
+    /*카카오*/
+    @Controller
+    public class KakaoController {
+        @Autowired
+        private KakaoService kakaoService;
+
+        @RequestMapping("/")
+        public String main(){
+            return "main";
+        }
+        @RequestMapping("/login")
+        public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
+            System.out.println("#########" + code);
+            String access_Token = kakaoService.getAccessToken(code);
+            HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
+            System.out.println("###userInfo#### : " + userInfo.get("email"));
+            System.out.println("###nickname#### : " + userInfo.get("nickname"));
+            System.out.println("###access_Token#### : " + access_Token);
+            return "redirect:/";//로그인 성공시 접속될 주소 입력하기
+        }
+    }
+
 }
