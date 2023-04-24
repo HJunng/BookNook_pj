f1d86efd7fae1f5c5e13732c49495485aa200b3f
hansumin <sumin_han@naver.com>
2023-04-25 01:16:44 +0900

    책 읽은 권수로 순위 나타내기 완료

--- src/test/java/com/hll/booknook/BooknookApplicationTests.java
+++ src/test/java/com/hll/booknook/BooknookApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hll.booknook;
 
+import com.hll.booknook.domain.Library;
+import com.hll.booknook.repository.LibraryRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BooknookApplicationTests {
 
+    @Autowired
+    private LibraryRepository libraryRepository;
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+
+//        Question q2 = new Question();
+//        q2.setSubject("스프링부트 모델 질문입니다.");
+//        q2.setContent("id는 자동으로 생성되나요?");
+//        q2.setCreateDate(LocalDateTime.now());
+//        this.questionRepository.save(q2);  // 두번째 질문 저장
     }
 
 }
