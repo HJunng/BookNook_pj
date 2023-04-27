825d3631adea24f16bbd65617c8b2d3230d0bdd3
Dahee <dduosy9@gmail.com>
2023-04-27 23:14:20 +0900

    book_detail 오류 해결(searchList.js, book_detail.js)

--- src/main/resources/static/js/book_detail.js
+++ src/main/resources/static/js/book_detail.js
@@ -1,8 +1,6 @@
 /* searchList.html에서 클릭한 책의 정보(isbn) 받기 */
-/*
 const urlParams = new URL(location.href).searchParams;
 const bookSearch = urlParams.get("bookSearch");
-*/
 
 $.ajax({
   method: "GET",
