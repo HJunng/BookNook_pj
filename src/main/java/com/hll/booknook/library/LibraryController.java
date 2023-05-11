cd2bbf44be552edaeaef8a008d912bd7427543d1
hansumin <sumin_han@naver.com>
2023-05-11 23:05:21 +0900

    [랭킹] 순위 10위까지 수정

--- src/main/java/com/hll/booknook/library/LibraryController.java
+++ src/main/java/com/hll/booknook/library/LibraryController.java
@@ -52,10 +52,12 @@ public class LibraryController {
             if (i<4){
                 toprk.add(data);
             }
-            else{
+            else if (i<11){
                 rk.add(data);
             }
-            //몇개 가져올까
+            else{
+                break;
+            }
             i += 1;
         }
         model.addAttribute("rankList", rk);
@@ -68,49 +70,6 @@ public class LibraryController {
     }
     @GetMapping("/bookDetail")
     public String book_detail(){ return "book_detail";}
-
-    @GetMapping("/books/ranking")//책 순위: 임시 페이지 원래는 redirect:/html/index.html에 나와야하는 기능
-    public String rank_index(Model model) {
-        List<Library> rankList = this.libraryService.getList();
-        //그룹화
-        Map<String, List<Library>> collect = rankList.stream().collect(groupingBy((Library::getEmail)));
-        Map<String, Integer> ranking = new HashMap<>();
-        //mapping된 아이디 별 읽은 책 권수를 ranking map 에 저장!
-        for (String key : collect.keySet()) {
-            int value = collect.get(key).toString().split(",").length;
-            ranking.put(key, value);
-        }
-        //순위: 비교할 수 있는 리스트로 전환
-        List<Entry<String, Integer>> list_entries = new ArrayList<>(ranking.entrySet());
-        //값 비교
-        Collections.sort(list_entries, (obj1, obj2) -> {
-            // 내림 차순 정렬
-            return obj2.getValue().compareTo(obj1.getValue());
-        });
-
-        //data 파라미터 보내기
-        List<HashMap<String, String>> rk = new ArrayList<>();
-        List<HashMap<String, String>> toprk = new ArrayList<>();
-        int i = 1;
-        for (Entry<String, Integer> entry : list_entries) {
-            HashMap<String, String> data = new HashMap<>();
-            String email = entry.getKey();
-            String nickname = email.substring(0,email.indexOf("@"));
-            data.put("nick", nickname);
-            data.put("cnt", String.valueOf(entry.getValue()));
-            if (i<4){
-                toprk.add(data);
-            }
-            else{
-                rk.add(data);
-            }
-            //몇개 가져올까
-            i += 1;
-        }
-        model.addAttribute("rankList", rk);
-        model.addAttribute("toprankList", toprk);
-        return "rank_list";
-    }
 
     @PostMapping("/books/input") // 책 넣기
     public String create(LibraryForm form){
